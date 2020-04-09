78baf6c0d39ec7eee6a53c5952dd76c765acfa30
Yang Fu Chia <kelly07122170@gmail.com>
2020-04-09 04:31:41 -0400

    -

--- log-data.php
+++ log-data.php
@@ -1,6 +1,8 @@
 <?php
   $log_file_name = 'data.log'; // Change to the log file name
-  $message = $_POST['say']; // incoming message
-  file_put_contents($log_file_name, $message, FILE_APPEND);
-  header('Location: /');
+  $say = $_POST['say']; // incoming message
+  file_put_contents($log_file_name, $say, FILE_APPEND);
+  //header('Location: /');
+
+  echo $say;
 >
