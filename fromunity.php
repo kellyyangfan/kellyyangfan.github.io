f1bcfd791c8364d45c1dec78909b201a24c30735
Yang Fu Chia <kelly07122170@gmail.com>
2020-04-17 12:09:12 -0400

    added php

---
+++ fromunity.php
@@ -0,0 +1,17 @@
+﻿<?php
+    $text1 = $_POST["time"];
+    $text2 = $_POST["attempt"];
+    
+    if($text1 != ""){
+        echo("Message sent successfully! ");
+        echo("Field 1: " . $text1);
+        echo("Field 2: " . $text2);
+        
+        $file = fopen("data.text", "a");
+        fwrite($file, $text1);
+        fwrite($file, $text2);
+        
+    }else{
+        echo("Message sending failed...");
+    }
+?>
--- log-data.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $log_file_name = 'data.log'; // Change to the log file name
-  $say = $_POST['say']; // incoming message
-  file_put_contents($log_file_name, $say, FILE_APPEND);
-  //header('Location: /');
-
-  echo $say;
->
